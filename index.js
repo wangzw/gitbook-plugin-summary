2374ff5f28e908b497f111bd9587e860ffe5f2f4
Zhanwei Wang <wang@hashdata.cn>
2018-03-11 00:09:51 +0800

    rewrite

--- index.js
+++ index.js
@@ -1,100 +1,81 @@
 const fs = require('fs'),
-      glob = require('glob'),
-      path = require('path'),
-      Parser = require('markdown-parser')
+  path = require('path'),
+  Parser = require('markdown-parser2')
 
-function generateEntry( title, path, isReadme, isFAQ ) {
-  if (isReadme && isFAQ)
-    return `## ${title}\n`
 
-  match = path.match(/\//g)
-  depth = 0
+function generate(parser, root, prefix, path, level) {
+  result = parser.parse(
+    fs.readFileSync(`${path}`, { encoding: 'utf8' }))
 
-  if (match) {
-    depth = match.length + (!isReadme && !isFAQ)
+  let title = 'unknown'
+
+  if (result.headings.length) {
+    title = result.headings[0].trim()
   }
 
-  if (depth <= 1 && isReadme) {
-    return `\n\n- [${title}](${path})\n`
-  }
+  path = path.substring(root.length + 1)
 
-  return `${Array(depth).join('    ')}- [${title}](${path})\n`
+  msg = `${Array(level).join('    ')}- [${prefix} ${title}](${path})\n`
+
+  return msg
 }
 
 module.exports = {
   hooks: {
     init: function () {
       const parser = new Parser(),
-            root = this.resolve(''),
-            bookTitle = this.config.get('title'),
-            readmeFilename = this.config.get('structure.readme'),
-            summaryFilename = this.config.get('structure.summary'),
-            isFAQ = this.config.get('plugins').includes('theme-faq')
+        root = this.resolve(''),
+        bookTitle = this.config.get('title'),
+        readmeFilename = this.config.get('structure.readme'),
+        summaryFilename = this.config.get('structure.summary')
 
       let ret = Promise.resolve(),
-          summaryContent = ( bookTitle ? `# ${bookTitle}\n\n` : '' )
+        summaryContent = (bookTitle ? `# ${bookTitle}` : '')
 
       const isReadme = path => path.includes(readmeFilename)
 
-      glob(
-        `**/*.md`,
-        {
-          cwd: root,
-          ignore: ['**/node_modules/**', `**/${summaryFilename}`],
-          nosort: true
-        },
-        ( err, files ) => {
-          files.sort((a, b) => {
-            if (a == b) {
-              return 0
-            }
-            if (isReadme(a) && path.dirname(a) == '.') {
-              return -1
-            }
-            if (path.dirname(b).includes(path.dirname(a)) && isReadme(a)) {
-              return -1
-            }
-            if (isReadme(b) && path.dirname(b) == '.') {
-              return 1
-            }
-            if (path.dirname(a).includes(path.dirname(b)) && isReadme(b)){
-              return 1
-            }
-            return a < b ? -1 : a > b ? 1 : 0
-          })
+      const walkSync = (dir, pattern, ignorelist = [], level = 1, prefix = '') => {
+        let index = level == 1 ? 1 : 0
 
-          files.forEach( ( filePath ) => {
-            ret = ret.then(
-              () => {
-                return new Promise(
-                  ( resolve, reject ) => {
-                    parser.parse(
-                      fs.readFileSync( `${root}/${filePath}`, { encoding: 'utf8' } ),
-                      ( err, result ) => {
-                        if ( result.headings.length ) {
-                          const fileTitle = result.headings[0].trim()
+        fs.readdirSync(dir).sort((a, b) => {
+          if (isReadme(a) && !isReadme(b)) {
+            return -1
+          } else if (!isReadme(a) && isReadme(b)) {
+            return 1
+          }
 
-                          summaryContent += generateEntry( fileTitle, filePath, isReadme(filePath), isFAQ )
-                        }
+          return a < b ? -1 : a > b ? 1 : 0
+        }).forEach(file => {
+          let isDir = fs.statSync(path.join(dir, file)).isDirectory()
 
-                        resolve()
-                      }
-                    )
-                  }
-                )
-              }
-            )
-          })
-
-          ret = ret.then(
-            () => {
-              fs.writeFileSync( `${root}/${summaryFilename}`, summaryContent, { encoding: 'utf8' } )
-
-              console.log(`\x1b[36mgitbook-plugin-summary: \x1b[32m${summaryFilename} generated successfully.`)
-
-              return Promise.resolve()
+          if ((file.match(pattern) && !ignorelist.includes(file)) || (isDir && !ignorelist.includes(file))) {
+            if (index == 0) {
+              adding = prefix
+              summaryContent += '\n\n'
+            } else {
+              adding = prefix ? prefix + '.' + index : '' + index
             }
-          )
+
+            if (isDir) {
+              walkSync(path.join(dir, file), pattern, ignorelist, level + 1, adding)
+            } else {
+              summaryContent += generate(parser, root, adding, path.join(dir, file), index == 0 ? level - 1 : level)
+            }
+
+            index = index + 1
+          }
+        })
+      };
+
+      walkSync(root, `\.md$`, ['node_modules', '.git', '_book', summaryFilename])
+
+      ret = ret.then(
+        () => {
+          fs.writeFileSync(`${root}/${summaryFilename}`, summaryContent, { encoding: 'utf8' })
+
+          console.log(`\x1b[36mgitbook-plugin-summary2: \x1b[32m${summaryFilename} generated successfully.`)
+
+          return Promise.resolve()
         }
       )
 
